a190de87f77faf4cc0292997a579883149df8fcd
janejyoo <janejaeyoo@gmail.com>
2018-03-21 21:36:06 -0400

    Added face recognition code snippets
    https://realpython.com/face-recognition-with-python/

---
+++ face_recog.py
@@ -0,0 +1,8 @@
+#face_recognition app
+#uses openCV
+#use cv2. (in console to look at available things)
+#dir(x) x is string
+#using https://realpython.com/face-recognition-with-python/
+
+
+import cv2
